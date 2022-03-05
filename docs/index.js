f848b6f8d9ddbbbb0acb41af5618c74ab6b6c815
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-03-04 23:34:26 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -4,6 +4,7 @@ btnOpen.addEventListener("click", function () {
   const myWindow = window.open("https://scotwatson.github.io/DisplayMessages/");
   myWindow.postMessage("Hello World!");
 });
+document.body.appendChild(btnOpen);
 window.addEventListener("message", function (e) {
   document.body.appendChild(document.createTextNode(e.data));
 });
