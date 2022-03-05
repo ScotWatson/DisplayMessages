bcc24d89f832051c95ee2fe3016170564fb815c6
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-03-05 14:59:50 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,5 @@
 console.log(window.opener)
+window.opener.location.href = "https://www.google.com";
 window.addEventListener("message", function (e) {
   e.source.location = "https://www.google.com";
   document.body.appendChild(document.createTextNode(e.data));
